ff303f376a302c73d6a0713bd1d8cb00bace27e3
Jonasz Karwacki <jonaszakr@gmail.com>
2020-01-08 11:21:56 -0800

    accept post requests

--- listen/views.py
+++ listen/views.py
@@ -1,12 +1,20 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
 
+from .forms import EmptyForm
+from .tasks import add
 
 class HomeView(TemplateView):
     template_name = "pages/home.html"
+    form_class = EmptyForm
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["signature"] = "issasignature"
         return context
+
+    def post(self, request, *args, **kwargs):
+        add.apply_async((2, 2))
+        return JsonResponse({"foo": "bar"})
