dc6f3be3a6444f9ef00318a492cebcdbcdc1f72e
Jonasz Karwacki <jonaszakr@gmail.com>
2020-01-08 11:21:42 -0800

    add url for about page

--- listen/urls.py
+++ listen/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import HomeView
+
 urlpatterns = [
     # home page
     path("", HomeView.as_view(), name="home"),
 
+    # about page
+    path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
 ]
