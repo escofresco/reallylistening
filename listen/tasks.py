328e3072337c64427f405f4fa17f310f23e7fa99
Jonasz Karwacki <jonaszakr@gmail.com>
2020-01-09 12:43:31 -0800

    add dummy task with delay

--- listen/tasks.py
+++ listen/tasks.py
@@ -1,6 +1,10 @@
+from __future__ import absolute_import, unicode_literals
 from config.celery_app import app
+from celery import shared_task
 
+import threading
 
 @app.task
 def add(x, y):
+    threading.Event().wait(5)
     return x + y
