fda8ff121d9e1f4beeb58b9eec4281237d2ec6af
Jonasz Karwacki <jonaszakr@gmail.com>
2020-01-09 12:44:18 -0800

    add view for results page

--- listen/views.py
+++ listen/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
 
@@ -8,14 +8,32 @@ from .tasks import add
 
 
 class HomeView(TemplateView):
-    template_name = "pages/home.html"
+    template_name = 'pages/home.html'
     form_class = EmptyForm
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["room_name"] = "test"
+        context['room_name'] = 'test'
         return context
 
     def post(self, request, *args, **kwargs):
-        add.apply_async((2, 2))
-        return JsonResponse({"foo": "bar"})
+        if 'file_url' in request.POST:
+            res = add.apply_async((2,2))
+            return JsonResponse({'task_id': res.id})
+
+class ListenedView(TemplateView):
+    '''Call pipeline with the task id passed by the url, then display results.
+    '''
+    template_name = 'pages/listened.html'
+
+    def get(self, request, task_id):
+        return render(request, self.template_name, {'task_id': task_id})
+
+    def post(self, request, *args, **kwargs):
+        return JsonResponse({'ready': True})
+        if 'task_id' in request.POST:
+            async_res = add.AsyncResult(request.POST['task_id'])
+
+            if async_res.ready():
+                return JsonResponse({'ready': True, 'res': async_res.get()})
+            return JsonResponse({'ready': False})
