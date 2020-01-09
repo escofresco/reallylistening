0d48314f6a309a2461da19b44efa3ad1701a4e96
Jonasz Karwacki <jonaszakr@gmail.com>
2020-01-09 12:43:18 -0800

    add new url for results page

--- listen/urls.py
+++ listen/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import HomeView
+from .views import HomeView, ListenedView
 
 urlpatterns = [
     # home page
-    path("", HomeView.as_view(), name="home"),
+    path('', HomeView.as_view(), name='home'),
+
+    path('<str:task_id>/', ListenedView.as_view(), name='listened-view'),
+
     # about page
     path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
+        'about/', TemplateView.as_view(template_name='pages/about.html'), name='about'
     ),
 ]
