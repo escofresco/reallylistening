d4e33b93d5a1f666849226e4bc58f09c10d6c431
Jonasz Karwacki <jonaszakr@gmail.com>
2020-01-08 17:09:18 -0800

    add test context var

--- listen/views.py
+++ listen/views.py
@@ -13,7 +13,7 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["signature"] = "issasignature"
+        context["room_name"] = "test"
         return context
 
     def post(self, request, *args, **kwargs):
