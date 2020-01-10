3b6ab83bd22a5de6622dac114503e0c69b1f780e
Jonasz Karwacki <jonaszakr@gmail.com>
2020-01-09 18:43:05 -0800

    fix credentials bug

--- listen/helpers.py
+++ listen/helpers.py
@@ -2,9 +2,10 @@ import os
 import requests
 import time
 import uuid
+import json
 
 import boto3
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError, NoCredentialsError
 
 
 def load_json_from_uri(uri):
@@ -34,11 +35,19 @@ def transcribe(job_uri):
             "FAILED",
         ]:
             break
-        print(status)
         time.sleep(5)
     return status
 
 
-def comprehend():
+def comprehend(text):
     """Wrapper for AWS Comprehend API"""
-    pass
+    comprehend = boto3.client(
+        "comprehend",
+        aws_access_key_id=os.getenv("DJANGO_AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("DJANGO_SECRET_ACCESS_KEY"),
+        region_name="us-west-1",
+    )
+    job_name = str(uuid.uuid4())
+    print('Calling DetectDominantLanguage')
+    print(json.dumps(comprehend.detect_dominant_language(Text = text), sort_keys=True, indent=4))
+    print("End of DetectDominantLanguage\n")
