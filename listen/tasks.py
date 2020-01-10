75353cccbbe5e26899268b86cb0bdbde7bf14a22
Jonasz Karwacki <jonaszakr@gmail.com>
2020-01-09 18:27:19 -0800

    add new task for main pipeline

--- listen/tasks.py
+++ listen/tasks.py
@@ -1,10 +1,26 @@
 from __future__ import absolute_import, unicode_literals
+
 from config.celery_app import app
 from celery import shared_task
+from .helpers import *
 
-import threading
+import time
+
 
 @shared_task
 def add(x, y):
     threading.Event().wait(5)
     return x + y
+
+
+@shared_task
+def start_listening(filename):
+    transcribe_res = transcribe(filename)
+    job = transcribe_res['TranscriptionJob']
+    if job['TranscriptionJobStatus'] == 'FAILED':
+        raise Exception('Transcript failed to complete')
+    transcribe_uri = job['Transcript']['TranscriptFileUri']
+    transcript_res = load_json_from_uri(transcribe_uri)
+    content = transcript_res['results']['transcripts'][0]['transcript']
+
+    return content
